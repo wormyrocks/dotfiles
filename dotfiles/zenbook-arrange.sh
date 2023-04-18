63dc092b42bbdcb798a6e78f7ac6eabdaff0ee1c
Evan Kahn <ekahn@hmc.edu>
2023-04-18 13:20:37 -0400

    fixed typo in zenbook-arrange

--- dotfiles/zenbook-arrange.sh
+++ dotfiles/zenbook-arrange.sh
@@ -22,7 +22,7 @@ calc () {
 get_matrix () {
   read -r A B a b c d <<< ${1}
   c0=$(calc "${a}/${A}")
-  c1=$(calc "${c}/${a}")
+  c1=$(calc "${c}/${A}")
   c2=$(calc "${b}/${B}")
   c3=$(calc "${d}/${B}")
   echo "${c0} 0 ${c1} 0 ${c2} ${c3} 0 0 1"
@@ -37,7 +37,7 @@ top_dims=$(get_mon_dims $top_screen_xrr_id)
 bot_dims=$(get_mon_dims $bot_screen_xrr_id)
 top_matrix=$(get_matrix "$total_dims $top_dims")
 bot_matrix=$(get_matrix "$total_dims $bot_dims")
-echo ${total_dims} -${top_dims} -${bot_dims}
+echo total: [${total_dims}] top: [${top_dims}] bottom: [${bot_dims}]
 ts_ids=$(get_xinput_ids_by ${top_screen_xi_id})
 bs_ids=$(get_xinput_ids_by ${bot_screen_xi_id})
 echo "top matrix: " ${top_matrix}
@@ -47,8 +47,9 @@ identity_matrix="1 0 0 0 1 0 0 0 1"
 for id in ${bs_ids}
 do
 	echo ${id} -\> ${bot_matrix}
-	xinput --set-prop ${id} --type=float "Coordinate Transformation Matrix" ${bot_matrix}
-	xinput --set-prop ${id} --type=float "libinput Calibration Matrix" ${identity_matrix}
+	xinput --set-prop ${id} --type=float "Coordinate Transformation Matrix" ${bot_matrix} && {
+		xinput --set-prop ${id} --type=float "libinput Calibration Matrix" ${identity_matrix}
+	} || xinput --disable ${id}
 done
 
 for id in ${ts_ids}
